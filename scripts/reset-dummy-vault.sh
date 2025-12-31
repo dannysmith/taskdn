84414cf137d23944af28acc4a609e7da6c38540a
Danny Smith <hi@danny.is>
2025-12-31 20:27:18 +0000

    Recreate symlink to obsidian plugin in dummy-demo-vault after at end of reset-dummy-vault.sh

--- scripts/reset-dummy-vault.sh
+++ scripts/reset-dummy-vault.sh
@@ -23,4 +23,23 @@ rm -rf "$TARGET"
 # Copy demo-vault to dummy-demo-vault
 cp -r "$SOURCE" "$TARGET"
 
+# Create symlink for local Obsidian plugin development
+PLUGIN_DIR="$TARGET/.obsidian/plugins"
+PLUGIN_LINK="$PLUGIN_DIR/obsidian-taskdn"
+PLUGIN_SOURCE="/Users/danny/dev/obsidian-taskdn"
+
+mkdir -p "$PLUGIN_DIR"
+
+if ln_error=$(ln -sf "$PLUGIN_SOURCE" "$PLUGIN_LINK" 2>&1); then
+  echo "Created symlink for obsidian-taskdn plugin"
+else
+  echo "Warning: Could not create symlink for obsidian-taskdn plugin"
+  echo "  $ln_error"
+  echo "  Source: $PLUGIN_SOURCE"
+  echo "  Target: $PLUGIN_LINK"
+fi
+
+echo ""
 echo "Done. dummy-demo-vault is ready for testing."
+OBSIDIAN_URL="obsidian://open?path=$TARGET"
+printf 'Open in Obsidian: \e]8;;%s\e\\%s\e]8;;\e\\\n' "$OBSIDIAN_URL" "$OBSIDIAN_URL"
