cd394328169caf6f7aa4b5d2f0655ac6b4ef9406
Danny Smith <hi@danny.is>
2026-01-19 01:48:08 +0000

    WIP Desktop Docs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -102,9 +102,6 @@ export default defineConfig({
             {
               slug: 'desktop/url-scheme',
             },
-            {
-              slug: 'desktop/keyboard-navigation',
-            },
           ],
         },
         {
